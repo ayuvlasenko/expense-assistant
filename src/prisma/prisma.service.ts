5a804edf7868f89882298a05174998416dfa75c0
Aleksandr Vlasenko <77232221+ayuvlasenko@users.noreply.github.com>
2023-09-14 15:59:15 +0400

    feat: update prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,9 @@
-import { INestApplication, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
+@Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit(): Promise<void> {
         await this.$connect();
-    }
-
-    // eslint-disable-next-line @typescript-eslint/require-await
-    async enableShutdownHooks(app: INestApplication): Promise<void> {
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        this.$on("beforeExit", async () => {
-            await app.close();
-        });
     }
 }
